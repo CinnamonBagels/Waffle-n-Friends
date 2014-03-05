05275e5f3cb4fd076a4a5f8896cef73319cb3253
Jeffrey Johnson <jeffcjohnson@live.com>
2014-03-04 19:11:56 -0800

    Scripts

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -1,13 +1,6 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class Hand : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
 	// Update is called once per frame
 	void Update () {
         //transform.Translate(Input.GetAxis("Horizontal"), 0, 0);
