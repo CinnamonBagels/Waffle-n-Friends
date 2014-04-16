0b5e787edcf0e59d7b9a895a45d7aa02ac5c33e0
patrickgh3 <patrickjtraynor@gmail.com>
2014-04-15 18:46:26 -0700

    Picking up objects

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Hand : MonoBehaviour {
     public float speed = 2f;
+
+    private GameObject Carrying;
+    private bool AbleToCarry = true;
 
     // Use this for initialization
     void Start () {
@@ -16,5 +20,41 @@ public class Hand : MonoBehaviour {
         Vector2 velocity = new Vector2( x, y );
 
         rigidbody2D.velocity = velocity;
+
+        // if pressed space, release object
+        if (Input.GetKeyDown(KeyCode.Space) && Carrying)
+        {
+            Carrying.transform.parent = null;
+            StartCoroutine(ForceOffMe(Carrying));
+            Carrying = null;
+            AbleToCarry = false;
+            StartCoroutine(MakeAbleToCarry());
+        }
+    }
+
+    IEnumerator ForceOffMe(GameObject obj)
+    {
+        obj.AddComponent<Rigidbody2D>();
+        yield return new WaitForFixedUpdate();
+        obj.GetComponent<Rigidbody2D>().AddForce(100000 * (obj.transform.position - transform.position));
+    }
+
+    IEnumerator MakeAbleToCarry()
+    {
+        yield return new WaitForSeconds(1.0f);
+        AbleToCarry = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (!Carrying && col.gameObject.tag == "Ingredient" && AbleToCarry)
+        {
+            Debug.Log(gameObject.name);
+            col.gameObject.transform.parent = gameObject.transform;
+            Carrying = col.gameObject;
+            Destroy(Carrying.GetComponent<Rigidbody2D>());
+            //Destroy(Carrying.GetComponent<Collider2D>());
+            //Carrying.GetComponent<Collider2D>().enabled = false;
+        }
     }
 }
